756883831a55d0a4ddd0b62294baafbb93c42e53
Marcus Brown <marcus.brown1234567@gmail.com>
2025-02-11 23:42:09 -0500

    add post and get fragment routes with raw-body

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -4,12 +4,15 @@
  * The main entry-point for the v1 version of the fragments API.
  */
 const express = require('express');
+const rawBody = require('../../raw-body');
+const { getFragments, getFragmentById } = require('./get');
 
 // Create a router on which to mount our API endpoints
 const router = express.Router();
 
-// Define our first route, which will be: GET /v1/fragments
-router.get('/fragments', require('./get'));
-// Other routes (POST, DELETE, etc.) will go here later on...
+router.get('/fragments', getFragments);
+router.get('/fragments/:id', getFragmentById);
+
+router.post('/fragments', rawBody(), require('./post'));
 
 module.exports = router;
