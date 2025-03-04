dc614485e5dbb772e1b18c6671b10f82645e1268
Marcus Brown <marcus.brown1234567@gmail.com>
2025-03-03 23:24:03 -0500

    add route for GET /fragments/:id/info

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -5,13 +5,14 @@
  */
 const express = require('express');
 const rawBody = require('../../raw-body');
-const { getFragments, getFragmentById } = require('./get');
+const { getFragments, getFragmentById, getFragmentInfo } = require('./get');
 
 // Create a router on which to mount our API endpoints
 const router = express.Router();
 
 router.get('/fragments', getFragments);
 router.get('/fragments/:id', getFragmentById);
+router.get('/fragments/:id/info', getFragmentInfo);
 
 router.post('/fragments', rawBody(), require('./post'));
 
