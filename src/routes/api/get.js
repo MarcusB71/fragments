72a1c4a676530bcb2a98423e931a619d7b836fac
Marcus Brown <marcus.brown1234567@gmail.com>
2025-03-20 15:55:57 -0400

    GET /v1/fragments/:id.ext now supports file type conversion with convertData and getExtensionType functions

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -3,10 +3,11 @@
 const { Fragment } = require('../../model/fragment');
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const logger = require('../../logger');
+const MarkdownIt = require('markdown-it');
+const md = new MarkdownIt();
+const sharp = require('sharp');
+const { htmlToText } = require('html-to-text');
 
-/**
- * Get a list of fragments for the current user
- */
 const getFragments = async (req, res) => {
   const ownerId = req.user; // Authenticated user (hashed email)
   const expand = req.query.expand;
@@ -28,8 +29,8 @@ const getFragments = async (req, res) => {
 };
 
 const getFragmentById = async (req, res) => {
-  const ownerId = req.user; // Authenticated user (hashed email)
-  const id = req.params.id; // Fragment ID from URL
+  const ownerId = req.user;
+  let [id, ext] = req.params.id.split('.');
 
   try {
     logger.debug(`Fetching fragment with ID: ${id} for user: ${ownerId}`);
@@ -40,16 +41,25 @@ const getFragmentById = async (req, res) => {
       logger.warn(`Fragment not found: ${id}`);
       return res.status(404).json(createErrorResponse(404, 'Fragment not found'));
     }
-    // Only allow `text/plain` retrieval
-    if (fragment.type !== 'text/plain') {
-      logger.warn(`Fragment ID ${id} is not text/plain, cannot retrieve`);
-      return res.status(415).json(createErrorResponse(415, 'Unsupported Media Type'));
-    }
 
     // Retrieve fragment data
     const data = await fragment.getData();
 
-    res.status(200).json(createSuccessResponse({ fragment: data.toString() }));
+    // Handle conversion if extension is provided
+    if (ext) {
+      const extType = getExtensionContentType(ext);
+      if (fragment.formats.includes(extType)) {
+        const convertedData = await convertData(data, fragment.mimeType, ext);
+        res.setHeader('Content-Type', extType);
+        res.status(200).send(convertedData);
+      } else {
+        logger.warn(`Unsupported extension: ${ext}`);
+        return res.status(415).json(createErrorResponse(415, 'Unsupported extension'));
+      }
+    } else {
+      res.setHeader('Content-Type', `${fragment.mimeType}`);
+      return res.status(200).send(data);
+    }
   } catch (err) {
     logger.error(`Error fetching fragment ${id} for user ${ownerId}: ${err.message}`);
     res.status(500).json(createErrorResponse(500, 'Internal Server Error'));
@@ -76,6 +86,109 @@ const getFragmentInfo = async (req, res) => {
     logger.error(`Error fetching metadata for fragment ${id} for user ${ownerId}: ${err.message}`);
     res.status(500).json(createErrorResponse(500, 'Internal Server Error'));
   }
+};
+
+const getExtensionContentType = (extension) => {
+  switch (extension) {
+    case 'txt':
+      return 'text/plain';
+    case 'md':
+      return 'text/markdown';
+    case 'html':
+      return 'text/html';
+    case 'json':
+      return 'application/json';
+    case 'png':
+      return 'image/png';
+    case 'jpg':
+      return 'image/jpeg';
+    case 'webp':
+      return 'image/webp';
+    case 'gif':
+      return 'image/gif';
+    default:
+      return null;
+  }
+};
+
+const convertData = async (data, from, to) => {
+  let convertedData = data;
+
+  switch (from) {
+    case 'text/markdown':
+      if (to == 'txt') {
+        convertedData = md.render(data.toString());
+        convertedData = htmlToText(convertedData.toString(), { wordwrap: 150 });
+        break;
+      }
+      if (to === 'html') {
+        convertedData = md.render(data.toString());
+      }
+      break;
+
+    case 'text/html':
+      if (to == 'txt') {
+        convertedData = htmlToText(data.toString(), { wordwrap: 130 });
+      }
+      break;
+
+    case 'application/json':
+      if (to == 'txt') {
+        convertedData = JSON.parse(data.toString());
+      }
+      break;
+
+    case 'image/png':
+      if (to == 'jpg') {
+        convertedData = await sharp(data).jpeg().toBuffer();
+      }
+      if (to == 'webp') {
+        convertedData = await sharp(data).webp().toBuffer();
+      }
+      if (to == 'gif') {
+        convertedData = await sharp(data).gif().toBuffer();
+      }
+      break;
+
+    case 'image/jpeg':
+      if (to == 'png') {
+        convertedData = await sharp(data).png().toBuffer();
+      }
+      if (to == 'webp') {
+        convertedData = await sharp(data).webp().toBuffer();
+      }
+      if (to == 'gif') {
+        convertedData = await sharp(data).gif().toBuffer();
+      }
+      break;
+
+    case 'image/webp':
+      if (to == 'png') {
+        convertedData = await sharp(data).png().toBuffer();
+      }
+      if (to == 'jpg') {
+        convertedData = await sharp(data).jpeg().toBuffer();
+      }
+      if (to == 'gif') {
+        convertedData = await sharp(data).gif().toBuffer();
+      }
+      break;
+
+    case 'image/gif':
+      if (to == 'png') {
+        convertedData = await sharp(data).png().toBuffer();
+      }
+      if (to == 'jpg') {
+        convertedData = await sharp(data).jpeg().toBuffer();
+      }
+      if (to == 'webp') {
+        convertedData = await sharp(data).webp().toBuffer();
+      }
+      break;
+  }
+
+  logger.debug(`Fragment data was successfully converted from ${from} to ${to}`);
+  return Promise.resolve(convertedData);
 };
 
 module.exports = {
