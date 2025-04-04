1c71ebf0faffafa531c7ad80b369a6a0afcecb2d
Marcus Brown <marcus.brown1234567@gmail.com>
2025-04-04 13:30:37 -0400

    update health check to include hostname

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,8 @@ const { version, author } = require('../../package.json');
 const { authenticate } = require('../auth');
 const { createSuccessResponse } = require('../response');
 
+const { hostname } = require('os');
+
 // Create a router that we can use to mount our API
 const router = express.Router();
 
@@ -23,6 +25,7 @@ router.use(`/v1`, authenticate(), require('./api'));
  * Define a simple health check route. If the server is running
  * we'll respond with a 200 OK.  If not, the server isn't healthy.
  */
+
 router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
@@ -32,6 +35,7 @@ router.get('/', (req, res) => {
       author,
       githubUrl: 'https://github.com/MarcusB71/fragments',
       version,
+      hostname: hostname(),
     })
   );
 });
